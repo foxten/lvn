28ac1a882988ab531f330324e22e1eea559ce4c3
Luis Pedraza <luis@lspdrz.com>
2023-01-09 21:47:09 +0100

    corrected id prefix

--- lvn_app/webhooks/main.py
+++ lvn_app/webhooks/main.py
@@ -51,7 +51,7 @@ def add_to_pbs(data):
         uid=data.uid,
     ).data
     (mvault_id, token) = register_user_on_pbs(
-        "lvn-" + user.uid,
+        "LVN-" + user.uid,
         {
             "first_name": user.first_name,
             "last_name": user.last_name,
