c6c025070a976037a282a6b2d2c23dd4f5de16f6
Luis Pedraza <luis@lspdrz.com>
2023-02-10 20:00:44 +0100

    fix piano api url

--- lvn_app/webhooks/main.py
+++ lvn_app/webhooks/main.py
@@ -15,7 +15,7 @@ so we're contacting the API directly here.
 def add_mvault_and_token_to_piano_user(mvault_id: str, token: str, user_id: str):
     requests.post(
         url=Config.PIANO_API_URL
-            +"/api/v3/publisher/user/update?aid="
+            +"/publisher/user/update?aid="
             +Config.PIANO_APP_ID
             +"&api_token="+Config.PIANO_API_TOKEN+"&uid="+user_id,
         headers={'Content-type': 'application/json'},
@@ -66,6 +66,7 @@ def add_to_pbs(data):
 def process_data(data):
     webhook_data = PIANO_CLIENT.parse_webhook_data(data)
     webhook_events = Config.PIANO_WEBHOOK_EVENTS.split(",")
-    if webhook_data.event in webhook_events and webhook_data.rid == Config.LV_PLUS_RESOURCE_ID:
+    # if webhook_data.event in webhook_events and webhook_data.rid == Config.LV_PLUS_RESOURCE_ID:
+    if (webhook_data.event == 'new_purchase' or webhook_data.event == 'free_access_granted') and webhook_data.rid == Config.LV_PLUS_RESOURCE_ID:
         add_to_pbs(webhook_data)
     return "User Registered to Mvault Successfully"
