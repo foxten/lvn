e7c1b4b19022190f4f07a60f718159cc94a78bf2
Daniel Kao <dkao@diplateevo.com>
2023-03-21 11:56:59 -0700

    unsubscribe_from_campaign_monitor

--- lvn_app/webhooks/main.py
+++ lvn_app/webhooks/main.py
@@ -306,7 +306,7 @@ def process_piano_webhook(request):
     elif request.method == 'POST':
         request_data = request.get_json()
         print(request_data.keys())
-        if 'user_removed' in request_data.keys():
+        if 'action' in request_data.keys() and request_data['action'] == 'user_removed':
             unsubscribe_from_campaign_monitor(request_data['email'])
 
     return "Webhook failed"
